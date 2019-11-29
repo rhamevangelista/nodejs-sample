1bb49c7b31c960f32b6a5e505b6e38ac9d38a4b0
rhamevangelista <ramilevangelista2009@gmail.com>
2019-11-29 08:55:57 +0000

    final

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
+app.route('/profile/:id').get(function(req, res){
+	const person = people.profiles.find(p => p.id === parseInt(req.params.id));
+	res.render('profile', {
+		title: `About ${person.name}`,
+		person,
+	});
+})
+
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Summit People',
-    people: people.profiles
-  });
+	res.render('index', {
+		title: 'Summit People',
+		people: people.profiles
+	});
 });
 
 app.listen(7000);
